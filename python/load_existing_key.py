24340e9e21eee5ea63de242ce4c47a086d9822af
ayaans16 <ashaik26@uoguelph.ca>
2026-07-18 13:32:21 -0400

    added pubkey generation function for priv keys

--- python/load_existing_key.py
+++ python/load_existing_key.py
@@ -8,7 +8,7 @@ This file is meant to take in existing private keys so you can:
 from pathlib import Path
 
 # imports for key detection
-from cryptography.hazmat.primitives.serialization import load_ssh_private_key
+from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_ssh_private_key
 from cryptography.hazmat.primitives.asymmetric import rsa, ec, ed25519
 
 from cryptography.hazmat.primitives import serialization
@@ -28,7 +28,10 @@ from cryptography.hazmat.primitives.asymmetric import ed25519
 for reference: -> str means the function return type should be a string
 """
 def get_key_type(key_bytes: bytes, password: bytes = None) -> str:
-    key = load_ssh_private_key(data=key_bytes, password=password)
+    try:
+        key = load_ssh_private_key(data=key_bytes, password=password)
+    except ValueError:
+        key = load_pem_private_key(data=key_bytes, password=password)
 
     if isinstance(key, rsa.RSAPrivateKey):
         return "RSA"
@@ -44,4 +47,35 @@ ssh_directory = Path("~/test_ssh/id_ed25519").expanduser()
 key_bytes = ssh_directory.read_bytes()
 key_type = get_key_type(key_bytes)
 print(key_type)
+"""
+
+def pubkey_generation(key_bytes: bytes, password: bytes = None) -> str:
+    key_type = get_key_type(key_bytes, password)
+
+    if key_type != "RSA" and key_type != "ED25519" and key_type != "ECDSA":
+        raise ValueError(f"RSA, ED25519 or ECDSA key expected, did not receive this")
+
+    if key_type == "ECDSA":
+        priv_key = load_pem_private_key(data=key_bytes, password=password)
+        public_key = priv_key.public_key()
+        public_pem = public_key.public_bytes(
+            encoding=Encoding.PEM,
+            format=PublicFormat.SubjectPublicKeyInfo
+        )
+
+    if key_type == "RSA" or key_type == "ED25519":
+        priv_key = load_ssh_private_key(data=key_bytes, password=password)
+        public_key = priv_key.public_key()
+        public_pem = public_key.public_bytes(
+            encoding=serialization.Encoding.OpenSSH,
+            format=serialization.PublicFormat.OpenSSH,
+        )
+
+    print(public_pem.decode())
+    return public_pem.decode()
+
+"""
+example usage:
+key_bytes = Path("~/test_ssh/id_ed25519").expanduser().read_bytes()
+pubkey_generation(key_bytes)
 """
